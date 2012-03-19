973c61aab9b48c8e1404538c30372b4b2efa3756
John Shumway <john.shumwayjr@gmail.com>
2012-03-19 16:40:12 -0700

    Set up a src/test subdirectory.

---
+++ src/test/Int8TypeTest.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include "Int8Type.h"
+
+namespace {
+
+class Int8TypeTest : public ::testing::Test {
+ protected:
+
+  Int8TypeTest() {
+    int8type = new Int8Type();
+  }
+
+  virtual ~Int8TypeTest() {
+    delete int8type;
+  }
+
+  virtual void SetUp() {}
+
+  virtual void TearDown() {}
+
+  const DataType* int8type;
+};
+
+TEST_F(Int8TypeTest, DoesXyz) {
+  // Exercises the Xyz feature of Foo.
+}
+}
